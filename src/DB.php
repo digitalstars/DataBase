ab9eae11348de80def679c9ac7603fbed5cb59ee
Runnin4ik <mr.kirilll12@mail.ru>
2024-02-04 22:01:04 +0300

    -- Минимальная версия php 8.0 -- Добавлено 5 попыток повторения запроса при наличие ошибки из конфига повторяемых запросов при ошибках. -- Добавлены новые коды ошибок, при которых запросы будут повторяться -- Добавлен метод setLoggerFunc() -- Добавлен метод execCommit(); -- Теперь библиотека наследуется от PDO, а не работает с его экземпляром. --

--- src/DB.php
+++ src/DB.php
@@ -3,139 +3,213 @@
 namespace DigitalStars\DataBase;
 
 use PDO;
-use function count;
-use function is_array;
-use function is_null;
+use PDOException;
 
-class DB extends \PDO
-{
+class DB {
     use Parser;
 
-    /** @var string $dsn */
-    private $dsn;
-    /** @var string $username */
-    private $username;
-    /** @var string $username */
-    private $passwd;
-    /** @var string $passwd */
-    private $options;
-    /** @var array|string $options */
+    private $anon_log_func;
 
-    /** @var PDO $pdo */
-    private $pdo;
+    private array $options;
 
-    public function __construct($dsn, $username = null, $passwd = null, $options = null) {
-        if (is_array($options))
+    public PDO $pdo;
+    
+    public array $error_codes_to_repeat = [1040, 1159, 1160, 1161, 2002, 2003, 2006, 1213]; //1158
+    
+    // 1040 - Too many connections
+    // 1158 - [Network error] Got a packet bigger than 'max_allowed_packet' bytes
+    // 1159 - [Network error] Got timeout reading communication packets
+    // 1160 - [Network error] Got an error reading communication packets
+    // 1161 - [Network error] Got packets out of order
+    // 2002 - Connection refused
+    // 2003 - Can't connect to MySQL server
+    // 2006 - MySQL server has gone away
+    // 1213 - Deadlock found when trying to get lock
+
+
+    public function __construct(private string $dsn, private ?string $username = null, private ?string $passwd = null, ?array $options = null) {
+        if (is_array($options)) {
             $options = array_replace([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION], $options);
-        else
+        }
+        else {
             $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-        $this->dsn = $dsn;
-        $this->username = $username;
-        $this->passwd = $passwd;
+        }
         $this->options = $options;
-        parent::__construct($dsn, $username, $passwd, $options);
+        for ($i = 1; $i <= 6; ++$i) {
+            try {
+                $this->pdo = new PDO($this->dsn, $this->username, $this->passwd, $options);
+            } catch (\PDOException $e) {
+
+                if($i <= 5) {
+                    $this->exceptionProcessing($e, $i);
+                    continue;
+                }
+
+                throw new \PDOException($e, $e->getCode(), $e);
+            }
+        }
     }
 
-    public static function create($dsn, $username = null, $passwd = null, $options = null) {
+    public function setLoggerFunc($func): void {
+        $this->anon_log_func = $func;
+    }
+
+    public function pdo() {
+        return $this->pdo;
+    }
+
+    public static function create($dsn, $username = null, $passwd = null, $options = null): DB {
         return new self($dsn, $username, $passwd, $options);
     }
 
-    public function exec($statement, $args = []) {
-        for ($i = 0; $i < 5; ++$i) {
+    public function exec(string $statement, array $args = []) {
+        for ($i = 1; $i <= 6; ++$i) {
+            $statement = (count($args) === 0 ? $statement : $this->parse($statement, $args));
             try {
-                return parent::exec(count($args) == 0 ? $statement : $this->parse($statement, $args));
+                $func = $this->anon_log_func;
+                if($func) {
+                    $func($statement . "\n-----------------");
+                }
+            } catch (Exception $e) {}
+            
+            try {
+                return $this->pdo->exec($statement);
             } catch (\PDOException $e) {
-                if ($i < 1 && ($e->errorInfo[1] == 2006 || $e->errorInfo[0] == 40001)) {
-                    if ($e->errorInfo[1] == 2006)
-                        parent::__construct($this->dsn, $this->username, $this->passwd, $this->options);
+                if($i <= 5) {
+                    $this->exceptionProcessing($e, $i);
                     continue;
                 }
-                throw new \PDOException($e);
+
+                throw new \PDOException($e, $e->getCode(), $e);
             }
         }
     }
 
-    public function query($statement, $args = [], $mode = null, $arg3 = null, $ctorargs = null) {
-        $statement = (count($args) == 0 ? $statement : $this->parse($statement, $args));
-        for ($i = 0; $i < 5; ++$i) {
+    public function execCommit(string $statement, array $args = []) {
+        try {
+            $this->pdo->beginTransaction(); // Начало транзакции
+            $result = $this->exec($statement, $args);
+            $this->pdo->commit(); // Фиксация изменений, если SQL-запрос выполнен успешно
+            return $result;
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack(); // Откат транзакции в случае ошибки
+            throw $e; // Переброс исключения
+        }
+    }
+
+    public function query(string $statement, array $args = [], $mode = null, $arg3 = null, $ctorargs = null) {
+        $statement = (count($args) === 0 ? $statement : $this->parse($statement, $args));
+        for ($i = 1; $i <= 6; ++$i) {
             try {
-                if (!is_null($ctorargs))
-                    return parent::query($statement, $mode, $arg3, $ctorargs);
-                else if (!is_null($arg3))
-                    return parent::query($statement, $mode, $arg3);
-                else if (!is_null($mode))
-                    return parent::query($statement, $mode);
-                return parent::query($statement);
-            } catch (\PDOException $e) {
-                if ($i < 1 && ($e->errorInfo[1] == 2006 || $e->errorInfo[0] == 40001)) {
-                    if ($e->errorInfo[1] == 2006)
-                        parent::__construct($this->dsn, $this->username, $this->passwd, $this->options);
+                $func = $this->anon_log_func;
+                if($func) {
+                    $func($statement . "\n-----------------");
+                }
+            } catch (Exception $e) {}
+            
+            try {
+                if (!is_null($ctorargs)) {
+                    $result = $this->pdo->query($statement, $mode, $arg3, $ctorargs);
+                } elseif (!is_null($arg3)) {
+                    $result = $this->pdo->query($statement, $mode, $arg3);
+                } elseif (!is_null($mode)) {
+                    $result = $this->pdo->query($statement, $mode);
+                } else {
+                    $result = $this->pdo->query($statement);
+                }
+                
+                return $result;
+            } catch (PDOException $e) {
+                if($i <= 5) {
+                    $this->exceptionProcessing($e, $i);
                     continue;
                 }
-                throw new \PDOException($e);
+
+                throw new PDOException($e, $e->getCode(), $e);
             }
         }
     }
 
-    public function prepare($statement, $args = [], array $driver_options = array()) {
-        return parent::prepare(count($args) == 0 ? $statement : $this->parse($statement, $args), $driver_options);
+    private function exceptionProcessing (\PDOException $e, int $i): void {
+        $msg = $e->getMessage();
+        if(!isset($e->errorInfo[1])) {
+            trigger_error($msg."\nНет кода ошибки. Попытка перезапуска запроса [{$i}/5]", E_USER_WARNING);
+            return;
+        }
+        
+        if (in_array($e->errorInfo[1], $this->error_codes_to_repeat) || $e->errorInfo[0] == 40001) {
+            if ($e->errorInfo[1] == 2006) {
+                trigger_error($msg."\nПопытка пересоздания конструктора и запуска запроса [{$i}/5]", E_USER_WARNING);
+                $this->__construct($this->dsn, $this->username, $this->passwd, $this->options);
+            } else {
+                trigger_error($msg."\nПопытка перезапуска запроса [{$i}/5]", E_USER_WARNING);
+            }
+        }
     }
 
-    public function rows($sql, $args = [], $fetchMode = PDO::FETCH_ASSOC) {
+    public function prepare(string $statement, array $args = [], array $driver_options = []) {
+        return $this->pdo->prepare(count($args) === 0 ? $statement : $this->parse($statement, $args), $driver_options);
+    }
+
+    public function rows(string $sql, array $args = [], $fetchMode = PDO::FETCH_ASSOC) {
         $result = $this->query($sql, $args);
-        return $result !== false ? $result->fetchAll($fetchMode) : $result;
+        $a = $result !== false ? $result->fetchAll($fetchMode) : false;
+        $result->closeCursor();
+        return $a;
     }
 
     public function row($sql, $args = [], $fetchMode = PDO::FETCH_ASSOC) {
         $result = $this->query($sql, $args);
-        return $result !== false ? $result->fetch($fetchMode) : $result;
+        return $result !== false ? $result->fetch($fetchMode) : false;
+    }
+
+    public function count(string $sql, array $args = []) {
+        $result = $this->query($sql, $args);
+        return $result !== false ? $result->rowCount() : false;
     }
 
     public function getById($table, $id, $fetchMode = PDO::FETCH_ASSOC) {
-        if (is_array($id))
+        if (is_array($id)) {
             $result = $this->query("SELECT * FROM ?f WHERE ?ws", [$table, $id]);
-        else
+        } else {
             $result = $this->query("SELECT * FROM ?f WHERE id = ?i", [$table, $id]);
-        return $result !== false ? $result->fetch($fetchMode) : $result;
-    }
-
-    public function count($sql, $args = []) {
-        $result = $this->query($sql, $args);
-        return $result !== false ? $result->rowCount() : $result;
+        }
+        
+        return $result !== false ? $result->fetch($fetchMode) : false;
     }
 
     public function insert($table, $data) {
         $result = $this->query("INSERT INTO ?f (?af) VALUES (?as)", [$table, array_keys($data), array_values($data)]);
-        return $result !== false ? $this->lastInsertId() : $result;
+        return $result !== false ? $this->pdo->lastInsertId() : false;
     }
 
     public function update($table, $data, $where = []) {
-        if (empty($where))
-            return $this->exec("UPDATE ?f SET ?As WHERE 1", [$table, $data]);
-        else
-            return $this->exec("UPDATE ?f SET ?As WHERE ?ws", [$table, $data, $where]);
+        if (empty($where)) {
+            return $this->execCommit("UPDATE ?f SET ?As WHERE 1", [$table, $data]);
+        }
+        return $this->execCommit("UPDATE ?f SET ?As WHERE ?ws", [$table, $data, $where]);
     }
 
     public function delete($table, $where, $limit = -1) {
-        if ($limit == -1)
-            return $this->exec("DELETE FROM ?f WHERE ?ws", [$table, $where]);
-        else
-            return $this->exec("DELETE FROM ?f WHERE ?ws LIMIT ?i", [$table, $where, $limit]);
+        if ($limit === -1) {
+            return $this->execCommit("DELETE FROM ?f WHERE ?ws", [$table, $where]);
+        }
+        return $this->execCommit("DELETE FROM ?f WHERE ?ws LIMIT ?i", [$table, $where, $limit]);
     }
 
     public function deleteAll($table) {
-        return $this->exec("DELETE FROM ?f", [$table]);
+        return $this->execCommit("DELETE FROM ?f", [$table]);
     }
 
     public function deleteById($table, $id) {
-        return $this->exec("DELETE FROM ?f WHERE id = ?i", [$table, $id]);
+        return $this->execCommit("DELETE FROM ?f WHERE id = ?i", [$table, $id]);
     }
 
     public function deleteByIds($table, $column, $ids) {
-        return $this->exec("DELETE FROM ?f WHERE ?f IN (?as)", [$table, $column, $ids]);
+        return $this->execCommit("DELETE FROM ?f WHERE ?f IN (?as)", [$table, $column, $ids]);
     }
 
     public function truncate($table) {
-        return $this->exec("TRUNCATE TABLE ?f", [$table]);
+        return $this->execCommit("TRUNCATE TABLE ?f", [$table]);
     }
 }
--- src/Parser.php
+++ src/Parser.php
@@ -6,17 +6,18 @@ namespace DigitalStars\DataBase;
 
 trait Parser {
     private $args;
-    private $original_query;
+    
+    private string $original_query;
+    
     private $result_query;
-    private $is_inner = false;
+    
+    private bool $is_inner = false;
 
-    public function parse($query, array $args) {
+    public function parse(string $query, array $args): string|array|null {
         $this->is_inner = false;
         $this->original_query = $query;
         $this->args = $args;
-        $anon = function ($v) {
-            return $this->parse_part($v);
-        };
+        $anon = fn($v) => $this->parse_part($v);
         $this->result_query = preg_replace_callback("!(\?[avw]\[.*?\])|(\?[avw]?[sidnf])!i", $anon, $query);
         return $this->result_query;
     }
@@ -26,16 +27,22 @@ trait Parser {
     }
 
     private function parse_part($symbol, $value = null) {
-        if (is_array($symbol))
+        if (is_array($symbol)) {
             $symbol = $symbol[0];
-        if ($symbol == '?')
+        }
+        
+        if ($symbol == '?') {
             return $symbol;
+        }
+        
         $symbol = trim($symbol);
-        if (!$this->is_inner and $symbol != '?n')
-            if (count($this->args) == 0)
+        if (!$this->is_inner && $symbol != '?n') {
+            if (count($this->args) == 0) {
                 throw new Exception('Несовпадение количества аргументов и заполнителей в массиве, запрос ' . $this->original_query);
-            else
-                $value = array_shift($this->args);
+            }
+            $value = array_shift($this->args);
+        }
+        
         switch ($symbol[1]) {
             case "S":
                 $is_like_escaping = true;
@@ -56,66 +63,76 @@ trait Parser {
                 if (isset($is_associative_array)) {
                     foreach ($value as $key => $val)
                         $result[] = $this->escapeFieldName($key) . "=" . $val;
-                    return join(',', $result);
-                } else
-                    return join(',', $value);
+                    
+                    return implode(',', $result);
+                }
+                return implode(',', $value);
             case "v":
-                if (!is_array($value))
+                if (!is_array($value)) {
                     throw new Exception($this->createErrorMessage('array', $value));
-                foreach ($value as $key => $val)
-                    $value[$key] = join(',', $this->parseArray($symbol, $value[$key]));
-                return "(".join("),(", $value).")";
+                }
+                
+                foreach (array_keys($value) as $key)
+                    $value[$key] = implode(',', $this->parseArray($symbol, $value[$key]));
+                
+                return "(".implode("),(", $value).")";
             case "w":
                 $value = $this->parseArray($symbol, $value);
                 foreach ($value as $key => $val)
                     $result[] = $this->escapeFieldName($key) . "=" . $val;
-                return join(' AND ', $result);
+                
+                return implode(' AND ', $result);
             default:
-                throw new Exception("Неизвестный заполнитель {$symbol[2]}");
+                throw new Exception('Неизвестный заполнитель ' . $symbol[2]);
 
         }
     }
 
-    private function parseArray($symbol, $value) {
-        if (!is_array($value))
+    private function parseArray($symbol, $value): array {
+        if (!is_array($value)) {
             throw new Exception($this->createErrorMessage('array', $value));
-        if ($symbol[2] == 'i')
+        }
+        
+        if ($symbol[2] == 'i') {
             foreach ($value as $key => $val)
                 $value[$key] = $this->getValueIntType($val);
-        else if ($symbol[2] == 'd')
+        } elseif ($symbol[2] == 'd') {
             foreach ($value as $key => $val)
                 $value[$key] = $this->getValueFloatType($val);
-        else if ($symbol[2] == 's')
+        } elseif ($symbol[2] == 's') {
             foreach ($value as $key => $val)
                 $value[$key] = $this->getValueStringType($val);
-        else if ($symbol[2] == 'f')
+        } elseif ($symbol[2] == 'f') {
             foreach ($value as $key => $val)
                 $value[$key] = $this->escapeFieldName($val);
-        else if ($symbol[2] == '[') {
+        } elseif ($symbol[2] == '[') {
             $selectors = explode(',',
                 substr($symbol, 3, strlen($symbol) - 4));
-            if (count($selectors) != count($value))
+            if (count($selectors) !== count($value)) {
                 throw new Exception('Несовпадение количества аргументов и заполнителей в массиве, запрос ' . $this->original_query);
+            }
+
             $this->is_inner = true;
             $index = 0;
             foreach ($value as $key => $val)
                 $value[$key] = $this->parse_part($selectors[$index++], $val);
+
             $this->is_inner = false;
-        } else
-            throw new Exception("Неизвестный заполнитель {$symbol[2]}");
+        } else {
+            throw new Exception('Неизвестный заполнитель ' . $symbol[2]);
+        }
+        
         return $value;
     }
 
-    private function escapeFieldName($value) {
+    private function escapeFieldName($value): string {
         if (!is_string($value)) {
             throw new Exception($this->createErrorMessage('field', $value));
         }
 
         $new_value = '';
 
-        $replace = function ($value) {
-            return '`' . str_replace("`", "``", $value) . '`';
-        };
+        $replace = static fn($value): string => '`' . str_replace("`", "``", $value) . '`';
 
         $dot = false;
 
@@ -134,33 +151,39 @@ trait Parser {
             }
 
             return rtrim($new_value, '.');
-        } else {
-            return $replace($value);
         }
+        return $replace($value);
     }
 
-    private function getValueIntType($value) {
-        if (is_integer($value))
+    private function getValueIntType($value): int|string {
+        if (is_int($value)) {
             return $value;
+        }
+        
         if (is_numeric($value) || is_bool($value)) {
             return (int)$value;
         }
+        
         if (is_null($value)) {
             return 'null';
         }
+        
         throw new Exception($this->createErrorMessage('integer', $value));
     }
 
-    private function getValueFloatType($value) {
+    private function getValueFloatType($value): float|string {
         if (is_float($value)) {
             return $value;
         }
+        
         if (is_numeric($value) || is_bool($value)) {
             return (float)$value;
         }
+        
         if (is_null($value)) {
             return 'null';
         }
+        
         throw new Exception($this->createErrorMessage('double', $value));
     }
 
@@ -169,9 +192,11 @@ trait Parser {
         if (is_bool($value)) {
             $value = (string)(int)$value;
         }
+        
         if (is_null($value)) {
             return 'null';
         }
+        
         if (!is_scalar($value)) {
             throw new Exception($this->createErrorMessage('string', $value));
         }
@@ -184,17 +209,17 @@ trait Parser {
         $var = $this->realEscapeString($var);
 
         if ($chars) {
-            $var = addCslashes($var, $chars);
+            return addCslashes($var, $chars);
         }
 
         return $var;
     }
 
     private function realEscapeString($value) {
-        return $this->quote($value);
+        return $this->pdo->quote($value);
     }
 
-    private function createErrorMessage($type, $value) {
-        return "Попытка указать для заполнителя типа $type значение типа " . gettype($value) . " в шаблоне запроса $this->original_query";
+    private function createErrorMessage($type, $value): string {
+        return sprintf('Попытка указать для заполнителя типа %s значение типа ', $type) . gettype($value) . (' в шаблоне запроса ' . $this->original_query);
     }
 }
